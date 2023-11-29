bb978cfb29817831c0cc21c346165b4d4f3a9c5a
john michael <johnmickey281@gmail.com>
2023-11-29 02:45:46 -0600

    productpage changes to include new propertied

--- lib/business/products.dart
+++ lib/business/products.dart
@@ -1,141 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:medfast_go/pages/home_page.dart';
 
-class Products extends StatelessWidget {
+class Products extends StatefulWidget {
+  @override
+  _ProductsState createState() => _ProductsState();
+}
+
+class _ProductsState extends State<Products> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
   final TextEditingController skuController = TextEditingController();
   final TextEditingController barcodeController = TextEditingController();
   final TextEditingController sellingPriceController = TextEditingController();
+  final TextEditingController medicineNameController = TextEditingController();
+  final TextEditingController medicineDescriptionController = TextEditingController();
+  final TextEditingController dosageFormController = TextEditingController();
+  final TextEditingController strengthController = TextEditingController();
+  final TextEditingController manufacturerController = TextEditingController();
+  final TextEditingController categoryController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController manufactureDateController = TextEditingController();
+  final TextEditingController expiryDateController = TextEditingController();
 
-  Products({super.key});
+  List<Map<String, dynamic>> products = [];
 
   void _showProductForm(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      builder: (BuildContext context) {
-        return SizedBox(
-          height: MediaQuery.of(context).size.height * 0.947,
+  showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    builder: (BuildContext context) {
+      // Get the total usable height excluding the app bar and status bar
+      final double totalHeight = MediaQuery.of(context).size.height;
+      final double statusBarHeight = MediaQuery.of(context).padding.top;
+      final double appBarHeight = AppBar().preferredSize.height;
+      final double maxSheetHeight = totalHeight - statusBarHeight - appBarHeight;
+      final double height = MediaQuery.of(context).size.height*0.86;
+      return ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: height, // Set the maximum height of the sheet
+        ),
+        child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 20.0),
-                  _customInputField(
-                      nameController, 'Product Name', 'Enter Product Name'),
-                  const SizedBox(height: 16.0),
-                  _customInputField(quantityController, 'Quantity in Stock',
-                      'Enter Quantity', TextInputType.number),
-                  const SizedBox(height: 16.0),
-                  _customInputField(skuController, 'SKU', 'Enter SKU'),
-                  const SizedBox(height: 16.0),
-                  _customInputField(barcodeController, 'Barcode',
-                      'Enter Barcode', TextInputType.number),
-                  const SizedBox(height: 16.0),
-                  _customInputField(
-                      sellingPriceController,
-                      'Selling Price',
-                      'Enter Selling Price',
-                      const TextInputType.numberWithOptions(decimal: true)),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  void _showMenuContainer(BuildContext context) {
-    showGeneralDialog(
-      context: context,
-      barrierDismissible: true,
-      barrierLabel: "Dismiss",
-      barrierColor: Colors.black.withOpacity(0.5),
-      transitionDuration: const Duration(milliseconds: 300),
-      pageBuilder: (context, anim1, anim2) {
-        return Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.3,
-            margin: const EdgeInsets.only(top: 50, left: 16.0, right: 16.0),
-            padding: const EdgeInsets.all(16.0),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(
-                Radius.circular(8.0),
-              ),
-            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: Text(
-                    'Product List',
-                    style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        decoration: TextDecoration.none),
-                  ),
-                ),
-                const SizedBox(height: 30.0),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      // Action for exporting to PDF/Excel
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(58, 205, 50, 1),
-                      textStyle: const TextStyle(fontSize: 20),
-                    ),
-                    icon: const Icon(Icons.insert_drive_file),
-                    label: const Text('Export as PDF/Excel'),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                const Divider(
-                    height: 30.0,
-                    thickness: 1.0,
-                    indent: 20.0,
-                    endIndent: 20.0), // This is the underline
-                const Spacer(), // Pushes the next widget to the end
-                Align(
-                  alignment: Alignment.bottomRight,
+                _customInputField(medicineNameController, 'Medicine Name', 'Enter Medicine Name'),
+                _customInputField(medicineDescriptionController, 'Medicine Description', 'Enter Medicine Description'),
+                _customInputField(dosageFormController, 'Dosage Form', 'Enter Dosage Form'),
+                _customInputField(strengthController, 'Strength', 'Enter Strength'),
+                _customInputField(manufacturerController, 'Manufacturer', 'Enter Manufacturer'),
+                _customInputField(categoryController, 'Category', 'Enter Category'),
+                _customInputField(priceController, 'Price', 'Enter Price'),
+                _datePickerField(manufactureDateController, 'Manufacture Date'),
+                _datePickerField(expiryDateController, 'Expiry Date'),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
                   child: ElevatedButton(
                     onPressed: () {
-                      Navigator.pop(context); // To close the dialog
+                      _saveProduct();
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
+                      primary: const Color.fromRGBO(58, 205, 50, 1),
                       textStyle: const TextStyle(fontSize: 20),
                     ),
-                    child: const Text('Close'),
+                    child: const Text('Save Product'),
                   ),
                 ),
               ],
             ),
           ),
-        );
-      },
-      transitionBuilder: (context, anim1, anim2, child) {
-        return SlideTransition(
-          position: Tween(begin: const Offset(0, -1), end: const Offset(0, 0))
-              .animate(anim1),
-          child: child,
-        );
-      },
-    );
+        ),
+      );
+    },
+  );
+}
+
+  void _saveProduct() {
+    setState(() {
+      products.add({
+        'medicineName': medicineNameController.text,
+        'medicineDescription': medicineDescriptionController.text,
+        'dosageForm': dosageFormController.text,
+        'strength': strengthController.text,
+        'manufacturer': manufacturerController.text,
+        'category': categoryController.text,
+        'price': priceController.text,
+        'manufactureDate': manufactureDateController.text,
+        'expiryDate': expiryDateController.text,
+      });
+
+      // Clear the text fields after saving
+      medicineNameController.clear();
+      medicineDescriptionController.clear();
+      dosageFormController.clear();
+      strengthController.clear();
+      manufacturerController.clear();
+      categoryController.clear();
+      priceController.clear();
+      manufactureDateController.clear();
+      expiryDateController.clear();
+    });
+
+    Navigator.pop(context); // Close the bottom sheet
   }
 
-  Widget _customInputField(
-      TextEditingController controller, String header, String hintText,
+  Widget _customInputField(TextEditingController controller, String header, String hintText,
       [TextInputType keyboardType = TextInputType.text]) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -153,21 +124,7 @@ class Products extends StatelessWidget {
           keyboardType: keyboardType,
           decoration: InputDecoration(
             hintText: hintText,
-            fillColor: Colors.white,
-            contentPadding: EdgeInsets.symmetric(
-              vertical: (header == 'Product Name') ? 35.0 : 20.0,
-              horizontal: 10.0,
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[300]!),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[300]!),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderSide:
-                  BorderSide(color: Color.fromRGBO(58, 205, 50, 1), width: 2.0),
-            ),
+            border: const OutlineInputBorder(),
             filled: true,
           ),
         ),
@@ -175,68 +132,111 @@ class Products extends StatelessWidget {
     );
   }
 
-  Widget buildEmptyMessage(BuildContext context) {
-    return Container(
-      height:
-          MediaQuery.of(context).size.height * 0.3, // 40% of the screen height
-      padding: const EdgeInsets.all(16.0),
-      margin: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(58, 205, 50, 0.1),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: const SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Hey!',
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
-            SizedBox(height: 10.0),
-            Text("You don't have any products to sell yet.",
-                style: TextStyle(fontSize: 20.0)),
-            SizedBox(height: 10.0),
-            Text(
-                "Let's add some products, it's easy to add, start tracking which products sold most or getting out of stock.",
-                style: TextStyle(fontSize: 20.0)),
-            SizedBox(height: 10.0),
-            Text('Click below + Icon to add your first product.',
-                style: TextStyle(fontSize: 20.0)),
-          ],
+  Widget _datePickerField(TextEditingController controller, String label) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        
+        Text(
+          label,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
         ),
-      ),
+        const SizedBox(height: 8.0),
+        TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: label,
+            border: const OutlineInputBorder(),
+            filled: true,
+          ),
+          onTap: () async {
+            DateTime? pickedDate = await showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime(2000),
+              lastDate: DateTime(2025),
+              builder: (BuildContext context, Widget? child) {
+                return Theme(
+                  data: ThemeData.light().copyWith(
+                    colorScheme: const ColorScheme.light().copyWith(
+                      primary: const Color.fromRGBO(58, 205, 50, 1),
+                    ),
+                  ),
+                  child: child!,
+                );
+              },
+            );
+            if (pickedDate != null) {
+              String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate);
+              controller.text = formattedDate;
+            }
+          },
+        ),
+      ],
     );
   }
 
-  @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Products'),
-          centerTitle: true,
-          backgroundColor: const Color.fromRGBO(58, 205, 50, 1),
-          leading: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (context) => const HomePage(),
-              ));
-            },
-            child: const Icon(Icons.arrow_back),
-          ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.menu), // This is the burger menu icon
-              onPressed: () {
-                _showMenuContainer(context);
-              },
-            ),
-          ],
-        ),
-        body: buildEmptyMessage(context),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _showProductForm(context);
-          },
-          backgroundColor: const Color.fromRGBO(58, 205, 50, 1),
-          child: const Icon(Icons.add),
+  Widget _buildProductList() {
+    if (products.isEmpty) {
+      return Center(
+        child: Text(
+          "No products added yet. Click the + button to add your first product.",
+          style: TextStyle(fontSize: 18.0),
         ),
       );
+    } else {
+      return ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          var product = products[index];
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text(product['medicineName'] ?? ''),
+              subtitle: Text('Description: ${product['medicineDescription'] ?? ''}, Price: ${product['price'] ?? ''}'),
+              // Add other fields if needed
+            ),
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(58, 205, 50, 1),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const HomePage(),
+            ));
+          },
+          child: const Icon(Icons.arrow_back),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              // Menu action
+            },
+          ),
+        ],
+      ),
+      body: _buildProductList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showProductForm(context);
+        },
+        backgroundColor: const Color.fromRGBO(58, 205, 50, 1),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
