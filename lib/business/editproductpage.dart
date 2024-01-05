5f6e51301050215e6d7bc0f3db15e350b095a055
Ja-ci <130248891+Ja-ci@users.noreply.github.com>
2024-01-05 16:03:15 +0300

    edit

--- lib/business/editproductpage.dart
+++ lib/business/editproductpage.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:medfast_go/data/DatabaseHelper.dart';
 import 'package:medfast_go/models/product.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class EditProductPage extends StatefulWidget {
   final Product product;
 
-  EditProductPage({required this.product});
+  const EditProductPage({Key? key, required this.product}) : super(key: key);
 
   @override
-  _EditProductPageState createState() => _EditProductPageState();
+  EditProductPageState createState() => EditProductPageState();
 }
 
-class _EditProductPageState extends State<EditProductPage> {
+class EditProductPageState extends State<EditProductPage> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
+  final TextEditingController expiryDateController = TextEditingController();
+
+  File? _image;
 
   @override
   void initState() {
     super.initState();
     // Initialize the text controllers with the product data
-    nameController.text = widget.product.productName ?? '';
-    descriptionController.text = widget.product.medicineDescription ?? '';
-    priceController.text = widget.product.buyingPrice.toString() ?? '';
+    nameController.text = widget.product.productName;
+    descriptionController.text = widget.product.medicineDescription;
+    priceController.text = widget.product.buyingPrice.toString();
+    expiryDateController.text = widget.product.expiryDate;
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    // ignore: deprecated_member_use
+    final pickedFile = await picker.getImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
   }
 
   void _updateProduct() async {
@@ -32,6 +50,7 @@ class _EditProductPageState extends State<EditProductPage> {
     final updatedName = nameController.text;
     final updatedDescription = descriptionController.text;
     final updatedPrice = double.parse(priceController.text);
+    final updatedExpiryDate = expiryDateController.text;
 
     // Create an updated Product object
     final updatedProduct = Product(
@@ -39,49 +58,128 @@ class _EditProductPageState extends State<EditProductPage> {
       productName: updatedName,
       medicineDescription: updatedDescription,
       buyingPrice: updatedPrice,
-      expiryDate: '',
+      expiryDate: updatedExpiryDate,
       manufactureDate: '',
+      image: _image?.path, // Update the image path
       // Add other fields as needed
     );
 
     // Update the product in the database
     await dbHelper.updateProduct(updatedProduct);
 
-    // Navigate back to the previous screen
-    Navigator.pop(context);
+    // Update the current widget with the new product data
+    setState(() {
+      widget.product.productName = updatedName;
+      widget.product.medicineDescription = updatedDescription;
+      widget.product.buyingPrice = updatedPrice;
+      widget.product.expiryDate = updatedExpiryDate;
+      widget.product.image = _image?.path;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Product'),
+        title: const Text('Edit Product'),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(58, 205, 50, 1),
+        backgroundColor: const Color.fromRGBO(58, 205, 50, 1),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            // Display the current image
+            _image != null
+                ? Image.file(
+                    _image!,
+                    height: 100,
+                    width: 100,
+                    fit: BoxFit.cover,
+                  )
+                : const SizedBox.shrink(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _pickImage(ImageSource.gallery),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Colors.green, // Set the button color to green
+                  ),
+                  child: const Text('Pick Image'),
+                ),
+                ElevatedButton(
+                  onPressed: () => _pickImage(ImageSource.camera),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Colors.green, // Set the button color to green
+                  ),
+                  child: const Text('Capture Image'),
+                ),
+              ],
+            ),
+            const SizedBox(
+                height: 16), // Add padding between image and text fields
             TextField(
               controller: nameController,
-              decoration: InputDecoration(labelText: 'Product Name'),
+              decoration: const InputDecoration(
+                labelText: 'Product Name',
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                ),
+                contentPadding:
+                    EdgeInsets.all(12.0), // Adjust padding as needed
+              ),
             ),
+            const SizedBox(height: 16), // Add padding between text fields
+            TextField(
+              controller: expiryDateController,
+              decoration: const InputDecoration(
+                labelText: 'Expiry Date',
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                ),
+                contentPadding:
+                    EdgeInsets.all(12.0), // Adjust padding as needed
+              ),
+            ),
+            const SizedBox(height: 16), // Add padding between text fields
             TextField(
               controller: descriptionController,
-              decoration: InputDecoration(labelText: 'Description'),
+              maxLines: 3, // Allow multiple lines
+              decoration: const InputDecoration(
+                labelText: 'Description',
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                ),
+                contentPadding:
+                    EdgeInsets.all(12.0), // Adjust padding as needed
+              ),
             ),
+            const SizedBox(height: 16), // Add padding between text fields
             TextField(
               controller: priceController,
-              decoration: InputDecoration(labelText: 'Price'),
               keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Price',
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green, width: 2.0),
+                ),
+                contentPadding:
+                    EdgeInsets.all(12.0), // Adjust padding as needed
+              ),
             ),
+            const SizedBox(height: 16), // Add padding between text fields
             ElevatedButton(
               onPressed: () {
                 _updateProduct();
               },
-              child: Text('Save Changes'),
-            
+              child: const Text('Save Changes'),
             ),
           ],
         ),
