d48c3d44429042aa4d6106b9e5e4556094e72154
johnmichael <johnmickey281@gmail.com>
2024-01-18 19:32:31 +0300

    Update on Sales Screen

--- lib/business/sales.dart
+++ lib/business/sales.dart
@@ -326,36 +326,39 @@ class _SalesState extends State<Sales> {
                 ),
               ),
               Expanded(child: _buildProductList()),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            OrderConfirmationScreen(cartItems: cartItems),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color.fromRGBO(114, 194, 117, 1),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    side: const BorderSide(color: Colors.black, width: 2.0),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 60, vertical: 10),
-                  ),
-                  child: const Text(
-                    'Confirm',
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
+Align(
+  alignment: Alignment.bottomCenter,
+  child: Padding(
+    padding: EdgeInsets.only(bottom: 0.25 * 150 / 2.54), // Adjusted bottom padding
+    child: ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) =>
+                OrderConfirmationScreen(cartItems: cartItems),
+          ),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        primary: const Color.fromRGBO(114, 194, 117, 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        side: const BorderSide(color: Colors.black, width: 2.0),
+        padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+      ),
+      child: const Text(
+        'Confirm',
+        style: TextStyle(
+            fontSize: 25,
+            color: Colors.black,
+            fontWeight: FontWeight.bold),
+      ),
+    ),
+  ),
+),
+
             ],
           ),
         ),
@@ -431,29 +434,82 @@ class _SalesState extends State<Sales> {
   }
 }
 
-class OrderConfirmationScreen extends StatelessWidget {
+
+class OrderConfirmationScreen extends StatefulWidget {
   final List<Product> cartItems;
-  final String orderNumber;
 
   OrderConfirmationScreen({Key? key, required this.cartItems})
-      : orderNumber = 'Order #${Random().nextInt(1000)}', // Random order number
-        super(key: key);
+      : super(key: key);
 
   @override
+  _OrderConfirmationScreenState createState() => _OrderConfirmationScreenState();
+}
+
+class _OrderConfirmationScreenState extends State<OrderConfirmationScreen> {
+  late Map<String, int> productQuantity;
+  late Map<String, double> productPrice;
+
+  @override
+  void initState() {
+    super.initState();
+    aggregateProductData();
+  }
+
+  void aggregateProductData() {
+    productQuantity = {};
+    productPrice = {};
+    for (var product in widget.cartItems) {
+      productQuantity[product.productName] = (productQuantity[product.productName] ?? 0) + 1;
+      
+      // Check if sellingPrice is not null before assigning
+      if (product.buyingPrice != null) {
+        productPrice[product.productName] = product.buyingPrice;
+      } else {
+        // Handle the case when sellingPrice is null, for example, set to 0.0 or some default value
+        productPrice[product.productName] = 0.0; // or some default value
+      }
+    }
+  }
+
+  void _removeItemFromCart(String productName) {
+    setState(() {
+      widget.cartItems.removeWhere((product) => product.productName == productName);
+      aggregateProductData(); // Re-aggregate data after removal
+    });
+  }
+
+
+    @override
   Widget build(BuildContext context) {
+  double getTotalPrice() {
+    double totalPrice = 0.0;
+    for (var product in widget.cartItems) {
+      if (productPrice[product.productName] == null) {
+        print("Price for ${product.productName} is null");
+      }
+      if (productQuantity[product.productName] == null) {
+        print("Quantity for ${product.productName} is null");
+      }
+
+      double price = productPrice[product.productName] ?? 0.0;
+      int quantity = productQuantity[product.productName] ?? 0;
+      totalPrice += price * quantity;
+    }
+    return totalPrice;
+  }
+
+  double totalPrice = getTotalPrice();
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(orderNumber),
+        title: Text('Order #${Random().nextInt(1000)}'), // Random order number
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
-        actionsIconTheme: IconThemeData(color: Colors.black),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
+          onPressed: () => Navigator.pop(context),
+        ),   actions: [
           IconButton(
             icon: Icon(Icons.search),
             onPressed: () {
@@ -468,57 +524,243 @@ class OrderConfirmationScreen extends StatelessWidget {
           headline6: TextStyle(color: Colors.black),
         ).headline6,
       ),
-      body: Center(
-        child: Container(
-          width: 500, // Set the desired width
-          height: 400, // Set the desired height
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(
-                'lib/assets/pharmacy-store.png', // Replace with the actual path to your background image
-              ),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                Color.fromARGB(255, 255, 255, 255)
-                    .withOpacity(0.1), // 10% transparency
-                BlendMode.dstATop,
+    body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Opacity(
+            opacity: 0.2,  // 20% transparency
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('lib/assets/pharmacy-store.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-            borderRadius: BorderRadius.circular(0),
           ),
-          child: ListView.builder(
-            itemCount: cartItems.length,
-            itemBuilder: (context, index) {
-              var product = cartItems[index];
-              return Column(
+          
+                   Positioned(
+  right: 10,
+  bottom: 10,
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.green,
+      border: Border.all(color: Colors.black, width: 2), // Black border
+      borderRadius: BorderRadius.circular(5), // Optional: if you want rounded corners
+    ),
+    padding: EdgeInsets.all(8),
+    child: Column(
+      mainAxisSize: MainAxisSize.min, // To fit the size to content
+      mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+      crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
+      children: [
+        Text(
+          'Total',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 16, // Adjust the font size as needed
+          ),
+        ),
+        Text(
+          'Ksh. ${totalPrice.toStringAsFixed(2)}',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 16, // Adjust the font size as needed
+          ),
+        ),
+      ],
+    ),
+  ),
+),
+
+          Positioned(
+  left: 10,
+  bottom: 10 + 1 * 38.1, // 1 cm above the bottom
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.black, // Black background for the container
+      borderRadius: BorderRadius.circular(5), // Rounded corners for the container
+    ),
+    padding: EdgeInsets.all(2), // Padding to create a border effect around the button
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.green,
+        elevation: 10, // Elevation for the button
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15), // Making the button a bit larger
+      ),
+      onPressed: () {
+        ;// Add your action for the sale button
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min, // To fit the row size to its children
+        children: [
+          Icon(Icons.shopping_bag, color: Colors.yellow), // Sale icon (bag) in yellow
+          SizedBox(width: 8), // Space between icon and text
+          Text(
+            'Sale',
+            style: TextStyle(
+              color: Colors.black, // Black text color
+              fontWeight: FontWeight.bold, // Bold text
+            ),
+          ),
+        ],
+      ),
+    ),
+  ),
+),
+Positioned(
+  right: 10,
+  bottom: 10 + 60, // Adjust this value as needed to position above the total price container
+  child: Column(
+    mainAxisSize: MainAxisSize.min,
+    crossAxisAlignment: CrossAxisAlignment.end, // Keeps the column aligned to the right
+    children: [
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 8), // Add horizontal padding
+        child: Center( // Centers the text horizontally in the container
+          child: Text(
+            'Add to order',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.normal,
+            ),
+          ),
+        ),
+      ),
+      Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        padding: EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white, // White background color for the button
+                onPrimary: Colors.black, // Text color (if you have text)
+              ),
+              onPressed: () {
+                // Add your action for the first button
+              },
+              child: Image.asset('lib/assets/bar-code.png', width: 30, height: 30), // Small image icon
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white, // White background color for the button
+                onPrimary: Colors.black, // Text color (if you have text)
+              ),
+              onPressed: () {
+                // Add your action for the second button
+              },
+              child: Image.asset('lib/assets/bar-code.png', width: 30, height: 30), // Small image icon
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+),
+
+
+          Align(
+            alignment: Alignment.topCenter,
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  ListTile(
-                    title: Text(
-                      product.productName,
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                  // Display the titles if the cart is not empty
+                  if (widget.cartItems.isNotEmpty)
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(child: Text('Item Name', textAlign: TextAlign.center)),
+                          Expanded(child: Text('Quantity', textAlign: TextAlign.center)),
+                          Expanded(child: Text('Price', textAlign: TextAlign.center)),
+                          Expanded(child: Text('Total', textAlign: TextAlign.center)),
+                        ],
+                      ),
                     ),
-                    subtitle: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Quantity: 1'), // Replace with actual quantity
-                        Text('Price: ${product.sellingPrice}'),
-                        Text(
-                            'Total: ${product.sellingPrice * 1}'), // Replace with actual total
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Text(
-                      'Expiry Date: ${product.expiryDate}',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),
-                  Divider(), // Add a divider between each product
+                  // Display each aggregated item's attributes
+                  ...productQuantity.entries.map((entry) {
+                    String productName = entry.key;
+                    int quantity = entry.value;
+                    double price = productPrice[productName] ?? 0.0;
+                    double total = quantity * price;
+
+                      return Container(
+                        margin: EdgeInsets.symmetric(vertical: 2.0),
+                        padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0), // Reduced vertical padding
+                        decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 3.0), // Increased border thickness
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Expanded(child: Text(productName, textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold))),
+                              Expanded(child: Text('$quantity', textAlign: TextAlign.center)),
+                              Expanded(child: Text('$price', textAlign: TextAlign.center)),
+                              Expanded(child: Text('$total', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold))),
+                              IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () => _removeItemFromCart(productName),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              'Expiry Date: ${widget.cartItems.firstWhere((product) => product.productName == productName).expiryDate}',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }).toList(),
                 ],
-              );
-            },
+              ),
+            ),
           ),
+        ],
+      ),
+    );
+  }
+}
+class MiniScreen extends StatelessWidget {
+  final VoidCallback onClose;
+
+  MiniScreen({Key? key, required this.onClose}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300, // Set your desired width
+        height: 200, // Set your desired height
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Mini Screen Content"),
+            ElevatedButton(
+              onPressed: onClose,
+              child: Text("Close"),
+            ),
+          ],
         ),
       ),
     );
