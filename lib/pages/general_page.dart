cfb691f738568d9b26c064bf078566ef8eeb23cb
BrianKawinzi <brian.mutisya@strathmore.edu>
2024-01-29 15:21:40 +0300

    icons and sales history added on general page

--- lib/pages/general_page.dart
+++ lib/pages/general_page.dart
@@ -6,6 +6,7 @@ import 'package:medfast_go/business/other_incomes.dart';
 import 'package:medfast_go/business/purchase_order.dart';
 import 'package:medfast_go/business/representative.dart';
 import 'package:medfast_go/business/sales.dart';
+import 'package:medfast_go/business/sale_order.dart';
 import 'package:medfast_go/business/stores.dart';
 import 'package:medfast_go/business/suppliers.dart';
 import 'package:medfast_go/pages/bottom_navigation.dart';
@@ -57,6 +58,10 @@ class GeneralPage extends StatelessWidget {
       case 'Other Income':
         Navigator.of(context)
           .push(MaterialPageRoute(builder: (context) => OtherIncome()));
+        break;
+      case 'Sale History':
+        Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => const SaleOrder()));
         break;
       default:
         Navigator.of(context)
@@ -124,46 +129,50 @@ class GeneralPage extends StatelessWidget {
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Sales"),
-            child: buildTile("sales")
+            child: buildTile("Sales", Icons.attach_money_outlined),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Expenses"),
-            child: buildTile("Expenses")
+            child: buildTile("Expenses", Icons.money_off),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Stores"),
-            child: buildTile("Stores")
+            child: buildTile("Stores", Icons.store),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Purchase Order"),
-            child: buildTile("Purchase Order")
+            child: buildTile("Purchase Order", Icons.shopping_cart),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Supplier"),
-            child: buildTile("Supplier")
+            child: buildTile("Supplier", Icons.person),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Activity"),
-            child: buildTile("Activity")
+            child: buildTile("Activity", Icons.notifications_active_outlined),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Customers"),
-            child: buildTile("Customers")
+            child: buildTile("Customers", Icons.people),
           ),
           GestureDetector
           (
             onTap: () => navigateToPage(context, "Representative"),
-            child: buildTile("Representative")
+            child: buildTile("Representative", Icons.person_add_alt_1_sharp),
+          ),
+          GestureDetector(
+            onTap: () => navigateToPage(context, "Sale History"),
+            child: buildTile("Sale History", Icons.receipt),
           ),
           GestureDetector(
             onTap: () => navigateToPage(context, "Other Income"),
-            child: buildLongTile("Other Income", context)
+            child: buildTile("Other Income", Icons.monetization_on),
           ),
         ],
       ),
