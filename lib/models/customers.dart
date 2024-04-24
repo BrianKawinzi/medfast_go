0f14f2d6f1620df91f046ddab2d8f35ab7f35988
Jayprodev <joshuamtunga20@gmail.com>
2024-04-25 02:19:49 +0300

    Customer Increment Update

--- lib/models/customers.dart
+++ lib/models/customers.dart
@@ -1,37 +1,42 @@
 class Customer {
-  int id; // This will be the primary key in the database
+  int? id; // This will be the primary key in the database
   String name;
   String contactNo;
   String emailAddress;
   String date;
 
   Customer({
-    this.id = 0,
+    this.id,
     required this.name,
     required this.contactNo,
     required this.emailAddress,
     required this.date,
   });
 
-  // Convert a Customer object into a Map object
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
+ Map<String, dynamic> toMap() {
+    var map = {
       'name': name,
       'contactNo': contactNo,
       'emailAddress': emailAddress,
       'date': date,
     };
+    
+    if (id != null) {
+      map['id'] = id as String; 
+    }
+    
+    return map;
   }
+
 
   // Convert a Map object into a Customer object
   factory Customer.fromMap(Map<String, dynamic> map) {
     return Customer(
-      id: map['id'],
-      name: map['name'],
-      contactNo: map['contactNo'],
-      emailAddress: map['emailAddress'],
-      date: map['date'],
+      id: map['id'] as int?,
+      name: map['name'] as String,
+      contactNo: map['contactNo'] as String,
+      emailAddress: map['emailAddress'] as String,
+      date: map['date'] as String,
     );
   }
 }
